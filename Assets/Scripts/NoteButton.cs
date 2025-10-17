b7670d999cd2887abe49caa16145599d838c853a
dangkhoa31204 <khoandse184762@fpt.edu.vn>
2025-10-17 14:39:24 +0700

    add NoteButton

---
+++ Assets/Scripts/NoteButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteButton : MonoBehaviour
+{
+    public static bool isNoteMode = false;
+    [SerializeField] private Image noteIcon;
+    [SerializeField] private Color activeColor = Color.yellow;
+    [SerializeField] private Color inactiveColor = Color.white;
+
+    void Start()
+    {
+        UpdateIcon();
+    }
+
+    public void ToggleNoteMode()
+    {
+        isNoteMode = !isNoteMode;
+        Debug.Log("Note mode: " + isNoteMode);
+        UpdateIcon();
+    }
+
+    void UpdateIcon()
+    {
+        if (noteIcon != null)
+            noteIcon.color = isNoteMode ? activeColor : inactiveColor;
+    }
+
+    public static bool IsNoteModeActive()
+    {
+        return isNoteMode;
+    }
+}
